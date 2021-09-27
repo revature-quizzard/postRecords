38f2ec2ac76740ff85b2f604849f50d4b21fe39d
hguilfoyle <hguilfoyle1@gmail.com>
2021-09-27 12:11:38 -0400

    filled in repository/records and handler logic
    Signed-off-by: hguilfoyle <hguilfoyle1@gmail.com>

--- src/main/java/com/revature/post_records/PostRecordsHandler.java
+++ src/main/java/com/revature/post_records/PostRecordsHandler.java
@@ -1,6 +1,7 @@
 package com.revature.post_records;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
@@ -10,10 +11,20 @@ import com.google.gson.GsonBuilder;
 public class PostRecordsHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
     private static final Gson mapper = new GsonBuilder().setPrettyPrinting().create();
-    private final RecordsRepository vgRepo = new RecordsRepository();
+    private final RecordsRepository recordRepo = new RecordsRepository();
 
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent, Context context) {
-        return null;
+        LambdaLogger logger = context.getLogger();
+
+        logger.log("RECEIVED EVENT: " + apiGatewayProxyRequestEvent);
+
+        Records newRecords =mapper.fromJson(apiGatewayProxyRequestEvent.getBody(), Records.class);
+
+        recordRepo.addRecord(newRecords);
+        APIGatewayProxyResponseEvent responseEvent = new APIGatewayProxyResponseEvent();
+        responseEvent.setBody(mapper.toJson(newRecords));
+
+        return responseEvent;
     }
 }
