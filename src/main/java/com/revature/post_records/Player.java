29f5669a762ec141408abbfa91f626092b21ccc8
hguilfoyle <hguilfoyle1@gmail.com>
2021-10-05 13:27:31 -0400

    new pojos for player and cards
    Signed-off-by: hguilfoyle <hguilfoyle1@gmail.com>

---
+++ src/main/java/com/revature/post_records/Player.java
@@ -0,0 +1,13 @@
+package com.revature.post_records;
+
+import lombok.Data;
+
+@Data
+public class Player {
+
+    private String id;
+    private String name;
+    private int placing;
+    private int points;
+
+}
